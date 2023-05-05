fafd99ce775e3dbc4b49cb734264d3e600a995d2
Ludo <ludovico.ferrara@ruhr-uni-bochum.de>
2023-05-05 20:50:14 +0200

    finished preview 0.1
    - added bootanimation transition
    - added icon, adaptive-icon, splash, favicon
    - added preview.apk

--- App.tsx
+++ App.tsx
@@ -1,23 +1,59 @@
-import React, { Component } from 'react';
-import { SafeAreaView } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { SafeAreaView, Animated, StyleSheet, Image } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { WebView } from 'react-native-webview';
 
 const userAgent = "Mozilla/5.0 (Linux; Android 4.1.1; Galaxy Nexus Build/JRO03C) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19"
 
-export default class MyWeb extends Component {
-    render() {
-        return (
-            <>
-                <StatusBar style="light" backgroundColor="#343541" translucent={false} />
-                <SafeAreaView style={{ flex: 1 }}>
-                    <WebView
-                        source={{ uri: 'https://chat.openai.com' }}
-                        userAgent={userAgent}
-                        sharedCookiesEnabled={true}
+const App = () => {
+    const fadeAnim = useRef(new Animated.Value(1)).current;
+    const zoomAnim = useRef(new Animated.Value(1)).current;
+
+    useEffect(() => {
+        Animated.parallel([
+            Animated.timing(fadeAnim, {
+                toValue: 0,
+                duration: 1000,
+                useNativeDriver: true,
+            }),
+            Animated.timing(zoomAnim, {
+                toValue: 25,
+                duration: 2000,
+                useNativeDriver: true,
+            }),
+        ]).start();
+    }, []);
+
+    return (
+        <>
+            <StatusBar style="light" backgroundColor="#343541" translucent={false} />
+            <SafeAreaView style={{ flex: 1 }}>
+                <WebView
+                    source={{ uri: 'https://chat.openai.com' }}
+                    userAgent={userAgent}
+                    sharedCookiesEnabled={true}
+                />
+                <Animated.View style={[
+                    StyleSheet.absoluteFill,
+                    {
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        opacity: fadeAnim,
+                        transform: [{ scale: zoomAnim }]
+                    },
+                ]}>
+                    <Image
+                        source={require('./assets/splash.png')}
+                        resizeMode="contain"
+                        style={{
+                            width: '100%',
+                            height: '100%',
+                        }}
                     />
-                </SafeAreaView>
-            </>
-        );
-    }
-}
+                </Animated.View>
+            </SafeAreaView>
+        </>
+    );
+};
+
+export default App;
